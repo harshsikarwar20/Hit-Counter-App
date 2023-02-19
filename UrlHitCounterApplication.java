25f3997cbcef005a0a8664c8b99e3cfc01f82a3c
Harsh Sikarwar <123385605+harshsikarwar20@users.noreply.github.com>
2023-02-20 00:19:24 +0530

    Create UrlHitCounterApplication.java

---
+++ UrlHitCounterApplication.java
@@ -0,0 +1,15 @@
+package com.example.urlHitCounterApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlHitCounterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlHitCounterAppApplication.class, args);
+		System.out.println("Done");
+	}
+
+
+}
